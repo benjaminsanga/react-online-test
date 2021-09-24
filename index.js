7b65981c468f14dd79363f6a0b4d1e5783136665
bilytaiba <benjotaiba@gmail.com>
2021-09-24 17:49:29 +0100

    serve interface build if in production

--- index.js
+++ index.js
@@ -58,6 +58,17 @@ app.post('/submit', (req, res) => {
     });
 });
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('interface/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'interface', 'build', 'index.html'));
+    });
+}
+
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
